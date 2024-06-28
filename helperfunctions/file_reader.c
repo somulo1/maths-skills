bc846821a2396694f9aef389524bd668be2ecb53
somulo <mcomulosammy37@gmail.com>
2024-06-28 16:11:13 +0300

    fix : read floating-point numbers

--- helperfunctions/file_reader.c
+++ helperfunctions/file_reader.c
@@ -1,11 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "helpers.h"
+#include <stdlib.h>
+#include <math.h>
 
+// Function to append an integer value to IntArray
 struct IntArray appendInt(struct IntArray array, int value) {
     array.size++;
     array.arr = realloc(array.arr, array.size * sizeof(int)); // Expand the array
-    array.arr[array.size-1] = value;
+    if (array.arr == NULL) {
+        perror("realloc");
+        exit(EXIT_FAILURE);
+    }
+    array.arr[array.size - 1] = value;
+    return array;
+}
+
+// Function to append a double value to IntArray
+struct IntArray appendDouble(struct IntArray array, double value) {
+    array.size++;
+    array.arr = realloc(array.arr, array.size * sizeof(int)); // Expand the array
+    if (array.arr == NULL) {
+        perror("realloc");
+        exit(EXIT_FAILURE);
+    }
+    array.arr[array.size - 1] = (int)value; // Truncate double to int for simplicity
     return array;
 }
 
@@ -23,19 +40,25 @@ int Read_data_from_file(const char *file_path, struct IntArray *data) {
 
     int i = 0;
     int num;
-    while (fscanf(file, "%d", &num) == 1) {
-        arr = appendInt(arr, num);
+    double dnum;
+    while (fscanf(file, "%lf", &dnum) == 1) {
+        num = (int)dnum; // Truncate double to int for simplicity
+        if (dnum == (double)num) {
+            arr = appendInt(arr, num);
+        } else {
+            arr = appendDouble(arr, dnum);
+        }
         i++;
     }
 
     if (!feof(file)) {
-        // fscanf failed to read an integer
-        printf("Error reading integer from file.Correct the file\n");
+        // fscanf failed to read a number
+        printf("Error reading number from file. Correct the file format.\n");
         return -1;
     }
 
     if (arr.size == 0) {
-        printf("File didn't contain any integers\n");
+        printf("File didn't contain any numbers.\n");
         return -1;
     }
 
@@ -44,11 +67,5 @@ int Read_data_from_file(const char *file_path, struct IntArray *data) {
     data->arr = arr.arr;
     data->size = arr.size;
 
-     // Print the array
-    // printf("Array: ");
-    // for (int i = 0; i < arr.size; i++) {
-    //     printf("%d ", arr.arr[i]);
-    // }
-    printf("\n");
     return i; // Return the number of elements read
 }
