a788925347decc4130659bd495fda4b0f71e94b8
somulo <mcomulosammy37@gmail.com>
2024-06-28 16:59:52 +0300

    fix: round-off output

--- helperfunctions/median.c
+++ helperfunctions/median.c
@@ -9,7 +9,7 @@ double Calculate_median(int data[], int n) {
     }
     qsort(temp, n, sizeof(int), Compare);
     if (n % 2 == 0) {
-        return (temp[n/2 - 1] + temp[n/2]) / 2.0;
+        return (round((temp[n/2 - 1] + temp[n/2]) / 2.0));
     } else {
         return temp[n/2];
     }
